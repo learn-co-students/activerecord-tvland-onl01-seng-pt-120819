543ee510a452de79c8a5a8541fa65c17f12ffccf
Damely Tineo <damely.tineo07@gmail.com>
2020-03-08 16:32:58 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,16 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters 
   
   def full_name
-    "#{self.first_name} #{self.last_name}"
+    "#{first_name} #{last_name}"
   end
   
   def list_roles
-    roles = []
-    
-    self.characters.each {|character|
-      roles << "#{character.name} - #{character.show.name}"
+    characters.map {|character|
+      "#{character.name} - #{character.show.name}"
     }
-    
-    roles 
   end
 end
