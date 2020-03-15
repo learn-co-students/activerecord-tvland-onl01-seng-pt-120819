a6e0c8ad0073324392a18f01f7daa22b7d4a374e
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-15 17:18:31 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,5 +2,9 @@ class Actor < ActiveRecord::Base
 
   has_many :characters
   has_many :shows, through: :characters
+#binding.pry
+  def full_name
+    full_name = "#{self.first_name} #{self.last_name}"
+  end
 
 end
